5b46d6fea74dcbbc01d58a2290accb8f59df5ae5
Daniel <dyk6704@vt.edu>
2024-05-07 14:59:12 -0400

    fixing RTD

--- docs/conf.py
+++ docs/conf.py
@@ -50,8 +50,8 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.intersphinx',
     'sphinx.ext.extlinks',
-    'sphinx.ext.sphinx_design',
-    'sphinx.ext.sphinx_copybutton',    
+    # 'sphinx_design',
+    'sphinx_copybutton',    
 ]
 
 
